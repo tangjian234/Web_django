28ad1119c3ab3b96897ca68150773c4a1ea6f0a2
Jian tang <tangjian234@gmail.com>
2020-06-03 10:08:42 -0700

    p

--- Code/Test/hello.py
+++ Code/Test/hello.py
@@ -4,10 +4,9 @@ import numpy as np
 import string_lib as s_lib
 import sys
 if __name__ == "__main__":
-  #help("modules")
-  #print(sys.path)
-  jlb.hello_json()
-  na = np.zeros((2,2)) 
-  print(na)
-  #blb.hello_basic()
-
+    # help("modules")
+    # print(sys.path)
+    jlb.hello_json()
+    na = np.zeros((2, 2))
+    print(na)
+    # blb.hello_basic()
